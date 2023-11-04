301f67a7f68732be242e07ee27c5ddc25d6c6998
rohan77988 <rohanchaudhari13579@gmail.com>
2023-11-04 23:09:14 +0530

    IM

--- placement_management/settings.py
+++ placement_management/settings.py
@@ -83,17 +83,26 @@ WSGI_APPLICATION = 'placement_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'placement_management',
+#         'USER': 'amit',
+#         'PASSWORD': 'pass123',
+#         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+#         'PORT': '5433',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'placement_management',
-        'USER': 'amit',
-        'PASSWORD': 'pass123',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '5432',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'CC-44Daaf3*2cBA4c2262dgCeD-*GECD',
+        'HOST': 'monorail.proxy.rlwy.net',   # Or an IP Address that your DB is hosted on
+        'PORT': '19312',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
